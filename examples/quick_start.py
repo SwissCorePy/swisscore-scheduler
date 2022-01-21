aa25b47d4539b74c5ba332003671fcb107279543
SwissCorePy <51398037+SwissCorePy@users.noreply.github.com>
2022-01-21 15:57:22 +0100

    Update quick_start.py

--- examples/quick_start.py
+++ examples/quick_start.py
@@ -5,21 +5,24 @@ from swisscore_scheduler import AsyncScheduler
 # creating the scheduler instance
 scheduler = AsyncScheduler()
 
+
 # creating a function or async function to schedule
 async def func(msg):
     print(f"Running {msg}!")
 
-# Run `func` each second with the msg argument beeing "each second"
-# This task runs forever until the scheduler or the task are stopped manually
+
+### schedule the same function in three different ways.
+
+# run `func` each second with the msg argument beeing "each second"
 scheduler.each.second.run(func, "each second")
 
-# Run `func` every 3 seconds with the msg argument beeing "every 3 seconds"
-# This task runs forever until the scheduler or the task are stopped manually
+# run `func` every 3 seconds with the msg argument beeing "every 3 seconds"
 scheduler.every(3).seconds.run(func, "every 3 seconds")
  
-# Run `func` once after 5 seconds with the msg argument beeing "once after 5 secondsd"
-# This task runs only once
+# run `func` once after 5 seconds with the msg argument beeing "once after 5 secondsd"
 scheduler.after(seconds=5).run(func, "once after 5 seconds")
+
 
 # starting the scheduler
 scheduler.start()
+
